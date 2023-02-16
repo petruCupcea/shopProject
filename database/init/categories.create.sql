2c22dd447acde0974a291e7662906cb4af9a8966
Cupcea Petru <petru.cupcea@getik.net>
2023-02-17 00:14:45 +0200

    work in progress

--- database/init/categories.create.sql
+++ database/init/categories.create.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `shop`.`categories` (
     `id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `name` VARCHAR(60) NOT NULL,
-    `type` VARCHAR(10) NOT NULL,
+    `type` VARCHAR(16) NOT NULL,
     `groupId` int
 );
