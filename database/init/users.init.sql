e05c41489b6c22f7219637b94711ae1b5c35515a
Cupcea Petru <petru.cupcea@getik.net>
2023-02-22 22:58:22 +0200

    login check works, next need to create session table

---
+++ database/init/users.init.sql
@@ -0,0 +1,2 @@
+INSERT INTO `shop`.`users` (`name`, `surname`, `email`, `phoneNumber`, `password`) VALUES
+            ('Petru', 'Cupcea', 'cupcea21@gmail.com', '069718832', '96cae35ce8a9b0244178bf28e4966c2ce1b8385723a96a6b838858cdd6ca0a1e')
